b1decaa27016f4efb16b58a6e979ef1964173615
jeromenavez <jerome.navez@student.uclouvain.be>
2017-05-09 12:51:20 +0200

    nettoyage 3

--- prolog.clj
+++ prolog.clj
@@ -1,9 +1,6 @@
-;;lein repl
-;;(load-file "prolog.clj")
-
 (ns prolog)
 
-(def memory (ref {}))
+(def database (ref {}))
 (def bindings (ref {}))
 
 (declare
@@ -18,6 +15,7 @@
 
 
 (defn set_bindings
+	"Resets the bindings with the `values'."
 	[values]
 	(dosync
 		(ref-set
@@ -25,6 +23,7 @@
 			values)))
 
 (defn add_bindings
+	"Adds a binding."
 	[key value]
 	(set_bindings
 		(merge
@@ -32,15 +31,18 @@
 				{key value})))
 
 (defn remove_bindings
+	"Removes a binding."
 	[key]
 	(set_bindings
 		(dissoc (deref bindings) key)))
 
 (defn vari?
+	"True if word is an variable in prolog."
 	[word]
 	(Character/isUpperCase (first (str word))))
 
 (defn val?
+	"True if word is an atom in prolog."
 	[word]
 	(Character/isLowerCase (first (str word))))
 
@@ -78,20 +80,20 @@
 			(rename_rules(rest rules)))))
 
 (defn in? 
-	"true if coll contains elm
+	"True if coll contains elm.
 	Source: http://stackoverflow.com/a/3249777/4203437"
 	[coll elm]  
 	(some
 		#(= elm %) coll))
 
 (defn add_rule
+	"Adds a new rule to the database."
 	[head body]
 	(dosync
 		(ref-set
-			memory
-			(merge-with
-				concat
-				(deref memory)
+			database
+			(merge-with concat
+				(deref database)
 				{(keyword (first head))
 					(list
 						(cons
@@ -100,7 +102,7 @@
 	nil)
 
 (defmacro <- 
-	"Define new rules and facts.
+	"Defines new rules and facts.
 	The first clause after is the head, the rest is the body (nothing for simple facts).
 	The head may contain any kind of structure (functors, atoms, ...), not only variables."
 	[head & body]
@@ -109,13 +111,15 @@
 		body))
 
 (defn get_rules
+	"Returns the rules associated to the `clause'."
 	[clause]
 	(rename_rules
 		(get
-			(deref memory)
+			(deref database)
 			(keyword (first clause)))))
 
 (defn key_of_val
+	"Returns the key related to the value `val' in the `bindings'."
 	[val keys]
 	(if (or (= keys nil) (= 0 (count keys)))
 		nil
@@ -124,7 +128,57 @@
 			(first keys)
 			(key_of_val val (rest keys)))))
 
+(defn get_last_value
+	"Returns the last value of a key by removing the intermediate bindings.
+	Example: in {:A X, :X F, :F R}, the last value of X is R.
+			{:A X, :X F, :F R} became {:A X} and R is returned."
+	[value]
+	(if (contains? (deref bindings) (keyword value))
+		(let [value2 (get (deref bindings) (keyword value))]
+			(remove_bindings (keyword value))
+			(get_last_value value2))
+		value))
+
+(defn clean_key
+	"Cleans a specific `key'."
+	[key]
+	(let [value (get (deref bindings) key)]
+		(if (or (= nil value) (val? value))
+			nil
+			(let [new_value (get_last_value value)]
+				(add_bindings key new_value)
+				nil))))
+
+(defn clean_keys
+	"Cleans the bindings to eliminate the intermediate variables.
+	Example: {:A X, :X F, :F R} becomes {:A R}.
+	Apply the cleaning on all the `keys'."
+	[keys]
+	(if (or (= nil keys) (= 0 (count keys)))
+		nil
+		(do
+			(clean_key (first keys))
+			(clean_keys (rest keys)))))
+
+(defn consistent
+	"True if the bindings are consistent.
+		* {:a b, :X Y} is not consistent because a is not equal to b?
+		* {:A a, :b B} is consistent."
+	[keys]
+	(if (or (= nil keys)
+			(= 0 (count keys)))
+		true
+		(if (and
+				(val? (.substring (str (first keys)) 1))
+				(val? (get (deref bindings) (first keys)))
+				(not=
+					(first keys)
+					(keyword (get (deref bindings) (first keys)))))
+			false
+			(consistent (rest keys)))))
+
 (defn can_be_bound
+	"True if the binding of var1 and var2 do not create duplicate values in the `bindings'."
 	[var1 var2]
 	(if (contains? (deref bindings) (keyword var1))
 		(if (and
@@ -136,59 +190,9 @@
 			false
 			true)))
 
-(defn rename_key
-	[key]
-	(if (contains? (deref bindings) key)
-		(rename_key (symbol (str (str key) "*")))
-		key))
-
-(defn rename_value
-	[value]
-	(if (val? value)
-		value
-		(if (in? (vals (deref bindings)) value)
-			(rename_key (symbol (str (str value) "*")))
-			value)))
-
-(defn get_last_value
-	[value]
-	(if (contains? (deref bindings) (keyword value))
-		(let [value2 (get (deref bindings) (keyword value))]
-			(remove_bindings (keyword value))
-			(get_last_value value2))
-		value))
-
-(defn clean_key
-	[key]
-	(let [value (get (deref bindings) key)]
-		(if (or (= nil value) (val? value))
-			nil
-			(let [new_value (get_last_value value)]
-				(add_bindings key new_value)
-				nil))))
-
-(defn clean_keys
-	[keys]
-	(if (or (= nil keys) (= 0 (count keys)))
-		nil
-		(do
-			(clean_key (first keys))
-			(clean_keys (rest keys)))))
-
-(defn consistent
-	[keys]
-	(if (or (= nil keys) (= 0 (count keys)))
-		true
-		(if (and
-				(val? (.substring (str (first keys)) 1))
-				(val? (get (deref bindings) (first keys)))
-				(not=
-					(first keys)
-					(keyword (get (deref bindings) (first keys)))))
-			false
-			(consistent (rest keys)))))
-
 (defn match
+	"Adds the binding {:var1 var2}
+	True only if the bindings are consistent."
 	[var1 var2]
 	(if (= (get (deref bindings) (keyword var1)) var2)
 		true
@@ -200,6 +204,8 @@
 			false)))
 
 (defn create_match
+	"True if var1 and var2 have been match.
+	False if var1 and var2 cannot be match."
 	[var1 var2]
 	(if (and (val? var1) (val? var2))
 	 	(if (= var1 var2)
@@ -214,7 +220,7 @@
 	 				false)))))
 
 (defn create_match_list
-	"Create the bindings between list1 and list2.
+	"Creates the bindings between list1 and list2.
 	* true if it can be bound
 	* false otherwise"
 	[list1 list2]
@@ -230,7 +236,7 @@
 			false)))
 
 (defn unify
-	"Test if the clause can be unified with the rule."
+	"Tests if the clause can be unified with the rule."
 	[clause rule]
 	(if-not (= (count clause)
 				(count (first rule)))
@@ -295,6 +301,7 @@
 						nexts))))))
 
 (defn to_keys
+	"Transform the list of `vars' in list of keys and remove the vars that are considered as atoms in prolog."
 	[vars]
 	(if (or (= nil vars) (= 0 (count vars)))
 		nil
@@ -304,41 +311,43 @@
 				(to_keys (rest vars)))
 			(to_keys (rest vars)))))
 
-(defn concat_in
-	[lists]
-	(if (or (= nil lists)
-			(= 0 (count lists)))
+(defn unknow_var
+	"Return the keys of the unknowns variables off the list of `clauses'."
+	[clauses]
+	(if (or (= nil clauses)
+			(= 0 (count clauses)))
 		nil
-		(concat
-			(to_keys (first lists))
-			(concat_in (rest lists)))))
+		(distinct (concat
+			(to_keys (first clauses))
+			(unknow_var (rest clauses))))))
 
-(defn keep_unknow
+(defn filter_unknow
+	"Removes the unwanted `bindings' to keep only the relevant ones."
 	[vars key_s]
 	(if (or (= nil key_s) (= 0 (count key_s)))
 		nil
 		(if-not (in? vars (first key_s))
 			(do
 				(remove_bindings (first key_s))
-				(keep_unknow vars (rest key_s)))
-			(keep_unknow vars (rest key_s)))))
+				(filter_unknow vars (rest key_s)))
+			(filter_unknow vars (rest key_s)))))
 
 (defmacro ?-
 	"Runs queries against the knowledge base.
 	It takes a variable number of goal clauses.
 	The effect is the same as separating the clauses with a comma in Prolog"
 	[& clauses]
-	(dosync(ref-set bindings {}))
+	(set_bindings {})
 	(if
 		(=(count clauses) 0)
 		'()
-		(let [result (prolog_and clauses) unknow (distinct (concat_in clauses))]
-			(keep_unknow unknow (keys (deref bindings)))
+		(let [result (prolog_and clauses) unknow (unknow_var clauses)]
+			(filter_unknow unknow (keys (deref bindings)))
 			(printf "Call:     %s\n" clauses)
 			(printf "Result:   %s\n" result)
 			(printf "Bindings: %s\n" (deref bindings))
 			(println)
 			(let [binds (deref bindings)]
-				(dosync(ref-set bindings {}))
+				(set_bindings {})
 				[result (str binds)]))))
 
